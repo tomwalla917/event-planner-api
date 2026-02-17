bceb952f3cd3a7f6a268c98e6350865ed8de491e
tomwalla917 <tywalla@gmail.com>
2026-02-16 19:55:08 -0500

    added get all and get by id exports to the eventcontroller file

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import {Event} from '../models/Events';
+import Event from '../models/Events';
 
 export const createEvent = async (req: Request, res: Response): Promise<void> =>{
     try{
@@ -18,7 +18,7 @@ export const createEvent = async (req: Request, res: Response): Promise<void> =>
 
         res.status(201).json({
             message: "Event created successfully",
-            post: savedEvent
+            event: savedEvent
         });
     }
     catch (err:any) {
@@ -30,3 +30,36 @@ export const createEvent = async (req: Request, res: Response): Promise<void> =>
     
 };
 
+export const getAllEvents = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const events = await Event.find();
+		
+		res.json({ 
+			count: events.length,
+			events 
+		});
+	} catch (err: any) {
+		res.status(500).json({ 
+			message: 'Error fetching events',
+			error: err.message 
+		});
+	}
+};
+
+export const getEventById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const event = await Event.findById(req.params.id);
+
+        if (!event) {
+            res.status(404).json({ message: "event not found"});
+            return;
+        }
+        res.json({event});
+    }
+     catch (err: any) {
+		res.status(400).json({ 
+			message: 'Invalid event ID',
+			error: err.message 
+		});
+	}
+};
